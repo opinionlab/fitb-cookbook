f610a245cb4ade7b2e967cba3b5dfccd0a9d1093
Adam Enger <adamenger@gmail.com>
2014-03-15 14:39:54 -0500

    adding comments

--- recipes/_apache.rb
+++ recipes/_apache.rb
@@ -1,11 +1,16 @@
+# install apache and mod_php5 for FITB
 include_recipe "apache2::default"
 include_recipe "apache2::mod_php5"
 
-package "php5-mysql"
+# only centos has a differently named mod_php5.so module, libphp5.so
+apache_module "php5" do
+  if node['platform'] == "centos"
+    filename "libphp5.so"
+  end
+end
 
-apache_module "php5"
-
-template "/etc/apache2/sites-available/fitb.conf" do
+# install template for fitb vhost in apache
+template "#{node['fitb']['apache']['conf_dir']}/fitb.conf" do
   source "fitb_vhost.erb"
   owner node['apache']['user']
   group node['apache']['group']
@@ -18,4 +23,5 @@ template "/etc/apache2/sites-available/fitb.conf" do
   notifies :reload, "service[apache2]", :delayed
 end
 
+# enable fitb vhost
 apache_site "fitb.conf"

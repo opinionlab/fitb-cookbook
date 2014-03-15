fd708c6972964e0408fe2caf1949d3a2c0d7b728
Adam Enger <adamenger@gmail.com>
2014-03-15 14:40:19 -0500

    split everything into own recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# use proper package management for distro
+case node["platform"]
+  when "debian", "ubuntu"
+    include_recipe "apt::default"
+  when "redhat", "centos", "fedora"
+    include_recipe "yum::default"
+    include_recipe "yum::epel"
+  end
+
 include_recipe "fitb::_mysql"
 include_recipe "fitb::_apache"
 include_recipe "fitb::_fitb"
